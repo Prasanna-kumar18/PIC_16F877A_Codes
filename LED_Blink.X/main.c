a06aea2ef68602a3c538630b72856573b032f909
Prasanna-kumar18 <s.v.prasannakumar1809@gmail.com>
2025-01-30 21:26:28 +0530

    LED Blink

---
+++ LED_Blink.X/main.c
@@ -0,0 +1,40 @@
+/*
+ * File:   main.c
+ * Author: Prasanna
+ *
+ * Created on 21 January, 2025, 7:33 PM
+ */
+
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = ON          // Flash Program Memory Code Protection bit (All program memory code-protected)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+#define _XTAL_FREQ 20000000     //20MHz Freqency oscillator
+
+void main(void) 
+{
+    
+    TRISB = 0x00;  //Entire Port b as Output pin
+ 
+    while(1)
+    {
+    PORTB = 0x0F;   //00001111
+    __delay_ms(1000);   //Delay for 1 Sec
+    PORTB = 0xF0;   //11110000
+    __delay_ms(1000);   //Delay for 1 Sec
+    }
+    
+    return;
+}
